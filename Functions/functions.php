0feee00ffc1e9b0b4fe0253eebd19e00ed8cc4b1
sunyuw <leentingOne@gmail.com>
2017-01-19 00:46:48 +0800

    move file

---
+++ Functions/functions.php
@@ -0,0 +1,5 @@
+<?php
+namespace finance_ifstrctr\Functions;
+
+require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'vendor'
+        . DIRECTORY_SEPARATOR . 'autoload.php';
--- src/Functions/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace finance_ifstrctr\Functions;
-
-function hello(){
-    echo "hello";
-}
